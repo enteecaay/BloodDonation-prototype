70e3799f51abc478e49dabb90da55ea481128a9e
Kiệt Nguyễn <kietnguyen060204@gmail.com>
2025-05-29 12:12:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/app/emergency
    > You cannot have two parallel pages that resolve to the same path.
    >

--- src/app/emergency/components/EmergencyClientPage.tsx
+++ src/app/emergency/components/EmergencyClientPage.tsx
@@ -2,5 +2,6 @@
 /*
 IMPORTANT: This component has been moved to /src/app/(authenticated)/emergency/components/EmergencyClientPage.tsx
 This file is now obsolete to prevent route conflicts and enforce authentication.
+It should not export a default component.
 */
 export {};
